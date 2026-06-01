291f451f03f596cd4136be0749ce5f12f2771a52
Rahul Nagrikar <rahulnagrikar123@gmail.com>
2026-06-01 23:30:47 +0530

    Update hero image to bright pink adenium flower for a lighter aesthetic

--- adenium-nursery/src/components/Hero.jsx
+++ adenium-nursery/src/components/Hero.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useRef } from 'react'
 import './Hero.css'
-import heroImage from '../assets/burgundy_hero.png'
+import heroImage from '../assets/bright_pink_hero.png'
 
 
 export default function Hero() {
